2e7c5c856b25c9ae84f11547c4aa685a961347ae
snek <40864710+snekplex@users.noreply.github.com>
2020-02-10 23:15:28 -0600

    Able to see uploaded file at /upload

--- server/app.js
+++ server/app.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const path = require('path');
+const fileUpload = require('express-fileupload');
 
 const app = express();
+const HOST = 'localhost';
+const PORT = 3001;
+
+const staticFilesPath = (path.join(__dirname, 'public'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload());
 
 app.get('/', (req, res) => {
-  res.json({'message': 'Express server live'});
+  res.json({ 'message': 'Express server live' });
 });
 
-app.listen(3000);
+app.post('/upload', (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    fileUploaded = false;
+    res.json({ 'fileUploaded': false });
+  } else {
+    const uploadedFiles = req.files;
+
+    res.json({ 'fileUploaded': true });
+  }
+});
+
+app.listen(PORT, HOST, () => console.log(`Running on http://${HOST}:${PORT}`));
